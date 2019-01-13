578870292ad562251025cc65390cccc6806fbb29
Andreas Alexander Maier <andb0t@pm.me>
2019-01-13 13:02:59 +0100

    Create more structure

--- recommender.py
+++ recommender.py
@@ -1,1 +1,12 @@
-print('Ha, there\'s nothing in it, yet!')
+"""The main file of the recommender."""
+
+
+def recommend():
+    """Recommend yes or no."""
+    result = True
+    print('The result is: {}'.format(result))
+    return result
+
+
+if __name__ == '__main__':
+    recommend()
